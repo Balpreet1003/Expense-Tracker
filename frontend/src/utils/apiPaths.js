6206e7ac663971d375a6602e5ad0fea4961f4d70
Balpreet1003 <balpreetsinghgill83@gmail.com>
2025-06-11 01:13:56 +0530

    api path update

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = "http://localhost:8000";
-export const BASE_URL = "https://expense-tracker-cnqa.vercel.app/";
+export const BASE_URL = "http://localhost:8000";
+//export const BASE_URL = "https://expense-tracker-cnqa.vercel.app/";
 
 // utils/apiPaths.js
 export const API_PATHS = {
