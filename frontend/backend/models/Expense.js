44382fd1464904dc66002defdf3805a572731fe9
Balpreet1003 <balpreetsinghgill83@gmail.com>
2025-06-27 14:49:50 +0530

    Updated forntend and backend

---
+++ frontend/backend/models/Expense.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const expenseSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    icon: String,
+    category: { type: String, required: true },
+    amount: { type: Number, required: true },
+    date: { type: Date, required: true }
+}); 
+ 
+module.exports = mongoose.model('Expense', expenseSchema);
