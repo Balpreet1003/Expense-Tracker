0dc6f716c9d1a9ea9e99eb483915452bfc582481
Balpreet1003 <balpreetsinghgill83@gmail.com>
2026-05-17 00:06:42 +0530

    Updating backend folder structure

--- backend/routes/incomeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { downloadIncomeExcel } = require('../controllers/incomeController');
-const { protect } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.get("/download-income", protect, downloadIncomeExcel);
-
-module.exports = router;
