c796687da9b67b666608d698828d62126596e1a4
Balpreet1003 <balpreetsinghgill83@gmail.com>
2026-08-24 22:19:49 +0530

    segregating server and client

--- backend/src/routes/aiRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { protect } = require('../middleware/authMiddleware');
-const { analyzeTransactions } = require('../controllers/aiController');
-
-const router = express.Router();
-
-router.post('/analyze', protect, analyzeTransactions);
-
-module.exports = router;
