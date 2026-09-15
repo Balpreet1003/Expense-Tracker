2d8048b20c4b27e37d24cc82b202d7057ad3b8f6
Balpreet1003 <balpreetsinghgill83@gmail.com>
2026-09-15 13:27:47 +0530

    fix(config): configure production API URLs

--- client/src/utils/apiPaths.js
+++ client/src/utils/apiPaths.js
@@ -1,6 +1,6 @@
-export const BASE_URL = import.meta.env.SERVER_URL;
+export const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
-export const AI_BASE_URL = import.meta.env.AI_BASE_URL;
+export const AI_BASE_URL = import.meta.env.VITE_AI_BASE_URL;
 
 export const API_PATHS = {
   AUTH: {
